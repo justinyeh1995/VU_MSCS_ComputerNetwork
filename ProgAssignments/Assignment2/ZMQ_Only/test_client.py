ed38527df79e571a25dde01c66290d85c751cb14
justinyeh1995 <justinyeh1995@gmail.com>
2022-10-20 22:08:42 -0500

    itegrated Hw2 routing table without layering

--- ProgAssignments/Assignment2/ZMQ_Only/test_client.py
+++ ProgAssignments/Assignment2/ZMQ_Only/test_client.py
@@ -21,6 +21,13 @@ import sys    # for system exception
 import time   # for sleep
 import argparse # for argument parsing
 import zmq    # this package must be imported for ZMQ to work
+import subprocess
+import re
+import pandas as pd
+
+testDB = pd.read_csv("./RouteDB.csv")
+ip2host = {f"10.0.0.{i}": f"H{i}" for i in range(1,10)}
+host2ip = { f"H{i}": f"10.0.0.{i}" for i in range(1,10)}
 
 ##################################
 # Driver program
@@ -52,8 +59,9 @@ def driver (args):
 
   try:
     # set our identity
-    print ("client setting its identity: {}".format (args.demux_token))
-    socket.setsockopt (zmq.IDENTITY, bytes (args.demux_token, "utf-8"))
+    final_addr = args.addr + ":" + str(args.port)
+    print ("client setting its identity: {}".format (final_addr))
+    socket.setsockopt (zmq.IDENTITY, bytes (final_addr, "utf-8"))
   except zmq.ZMQError as err:
     print ("ZeroMQ Error setting sockopt: {}".format (err))
     return
@@ -62,9 +70,24 @@ def driver (args):
     return
 
   try:
+    ## Get host ip & convert it to host name
+    print("Obtaining the Next Hop Host Name")
+    ifconfig_output=(subprocess.check_output('ifconfig')).decode()
+    regex_ip=re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}",ifconfig_output)
+    my_ip = str(regex_ip.group(0))
+    #assert my_ip == args.myaddr # just to make sure it's same, i will clean this up later
+    ## @TO-DO@
+    nexthost = testDB.loc[testDB.host==ip2host[my_ip]].loc[testDB.destination==str(args.addr)].nexthop.values[0] # replace "10.0.0.5" with final destination
+    nexthop = host2ip[nexthost]
+    print(f"Router HostName is {nexthop}")
+    print(f"Router Host IP is {ip2host[my_ip]}")
+  except:
+    print ("Some exception occurred getting ROUTER host name...")
+    return
+  try:
     # as in a traditional socket, tell the system what IP addr and port are we
     # going to connect to. Here, we are using TCP sockets.
-    connect_string = "tcp://" + args.addr + ":" + str (args.port)
+    connect_string = "tcp://" + nexthop + ":" + str (4444)
     print ("TCP client will be connecting to {}".format (connect_string))
     socket.connect (connect_string)
   except zmq.ZMQError as err:
@@ -117,7 +140,7 @@ def parseCmdLineArgs ():
   # add optional arguments
   parser.add_argument ("-a", "--addr", default="127.0.0.1", help="IP Address of next hop router to connect to (default: localhost i.e., 127.0.0.1)")
   parser.add_argument ("-p", "--port", type=int, default=4444, help="Port that next hop router is listening on (default: 4444)")
-  parser.add_argument ("-i", "--iters", type=int, default=1000, help="Number of iterations (default: 1000")
+  parser.add_argument ("-i", "--iters", type=int, default=1, help="Number of iterations (default: 1000")
   parser.add_argument ("-m", "--message", default="HelloWorld", help="Message to send: default HelloWorld")
   parser.add_argument ("-t", "--demux_token", default="client", help="Our identity used as a demultiplexing token (default: client)")
   args = parser.parse_args ()
